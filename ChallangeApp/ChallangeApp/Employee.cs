78839549d7e3aa939357ed2f2807a12a6f9e92bb
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-10-19 01:59:16 +0200

    clean code

--- ChallangeApp/ChallangeApp/Employee.cs
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -2,7 +2,7 @@
 {
     public class Employee
     {
-       
+
         private List<int> points = new List<int>();
         public Employee(string name, string surname, int age)
         {
@@ -10,13 +10,11 @@
             this.Surname = surname;
             this.Age = age;
         }
-
-
         public string Name { get; private set; }
 
         public string Surname { get; private set; }
 
-        public int Age { get; private set;}
+        public int Age { get; private set; }
 
         public int Result
         {
@@ -30,8 +28,5 @@
         {
             this.points.Add(number);
         }
-
-
-
     }
 }
