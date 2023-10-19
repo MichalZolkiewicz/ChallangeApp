c9a97f845cf9e2207da683bd9016e44cde4713b9
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-10-19 17:04:49 +0200

    zadanie domowe dzień 7

---
+++ ChallangeApp/ChallangeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,38 @@
+namespace ChallangeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenEmployeeColletScores_ShouldCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Micha³", "Zolkiewicz", 32);
+            employee.AddScore(4);
+            employee.AddScore(5);
+            employee.AddScore(-1);
+            employee.AddScore(-8);
+
+            // act
+            var result = employee.Result;
+
+            // assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void WhenScoreIsRemoved_ShouldCorrectResult()
+        {
+            //arrange
+            var employee = new Employee("Micha³", "Zolkiewicz", 32);
+
+            //act
+            employee.AddScore(4);
+            employee.AddScore(5);
+            employee.RemoveScore(5);
+            employee.RemoveScore(4);
+
+            //assert
+            Assert.AreEqual(0, employee.Result);
+        }
+    }
+}
